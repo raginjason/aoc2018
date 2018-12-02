37931d901f96f868d97a7fd219c9ad7c6f616aa6
Jason Walker <ragin.jason@gmail.com>
2018-12-02 14:10:21 -0500

    Changed day1 to have a function

--- day1.go
+++ day1.go
@@ -1,10 +1,34 @@
 package main
 
+import (
+	"log"
+	"strconv"
+)
+
 // Sum variadic sum function
 func Sum(numbers ...int) int {
 	var total int
 	for _, number := range numbers {
 		total = total + number
 	}
+	return total
+}
+
+func day1() int {
+	lines, err := FileInput(1)
+	if err != nil {
+		log.Fatalf("failed to get data, %s", err)
+	}
+	var total int
+
+	for _, line := range lines {
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatalf("could not convert %s, %s\n", line, err)
+		}
+		log.Printf("current: %d, add: %d\n", total, num)
+		total = Sum(total, num)
+	}
+
 	return total
 }
